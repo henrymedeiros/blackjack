61315ecda32c495585a88bfd7b4bb78745f69918
Henry Medeiros <henrymedeiros77@gmail.com>
2020-09-24 19:33:11 -0300

    Estruturando a página e adicionando os assets iniciais

---
+++ deck.js
@@ -0,0 +1,62 @@
+
+var cards = [
+    {
+        "name": "2",
+        "img": "assets/cards/2.svg",
+        "value": 2
+    },
+    {
+        "name": "3",
+        "img": "assets/cards/3.svg",
+        "value": 3
+    },
+    {
+        "name": "4",
+        "img": "4.svg",
+        "value": 4    },
+    {
+        "name": "5",
+        "img": "5.svg",
+        "value": 5
+    },
+    {
+        "name": "6",
+        "img": "6.svg",
+        "value": 6
+    },
+    {
+        "name": "7",
+        "img": "7.svg",
+        "value": 78    },
+    {
+        "name": "8",
+        "img": "8.svg",
+        "value": 8
+    },
+    {
+        "name": "9",
+        "img": "9.svg",
+        "value": 9
+    },
+    {
+        "name": "king",
+        "img": "K.svg",
+        "value": 10
+    },
+    {
+        "name": "jack",
+        "img": "J.svg",
+        "value": 10
+    },
+    {
+        "name": "queen",
+        "img": "Q.svg",
+        "value": 10
+    },
+    {
+        "name": "ace",
+        "img": "A.svg",
+        "value": 11
+    },
+]
+export {cards};
---
+++ scripts.js
@@ -0,0 +1,4 @@
+
+import {cards} from './deck.js'
+console.log(cards[0]);
+
